d212dd5e72086a6a897b29a7327bd64912576979
barry <onlylonger@outlook.com>
2025-07-04 23:13:50 +0800

    U

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,20 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Barry Portal";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const menuList = [
+  {
+    label: "Home",
+    url: "/",
+  },
+  {
+    label: "Project",
+    url: "/project",
+  },
+  {
+    label: "Blog",
+    url: "/blog",
+  },
+];
